dffa10f65083c96358b8f67b56ab98d39d68151f
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-07-22 16:34:03 +0100

    init Project

---
+++ src/test/java/fr/mkadia/mkadiaapi/MkadiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mkadia.mkadiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkadiaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
