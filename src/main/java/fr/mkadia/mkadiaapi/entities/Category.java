f69eb7b5b543e664d8b21001f13395c1fee9dfca
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-10-09 23:08:50 +0100

    DDL Category with Migration V2 And Trigger for invoked on update

--- src/main/java/fr/mkadia/mkadiaapi/entities/Category.java
+++ src/main/java/fr/mkadia/mkadiaapi/entities/Category.java
@@ -1,2 +1,27 @@
-package fr.mkadia.mkadiaapi.entities;public class Category {
+package fr.mkadia.mkadiaapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor @NoArgsConstructor @Builder @Getter @Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    private String name;
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
