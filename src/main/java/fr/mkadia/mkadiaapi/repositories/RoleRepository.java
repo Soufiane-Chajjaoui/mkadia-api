79748e750bb8465e6427af8433bae975368ba2ad
Soufiane Chajjaoui <schajjaoui@gmail.com>
2025-05-15 15:13:42 +0100

    dockerize all services

--- src/main/java/fr/mkadia/mkadiaapi/repositories/RoleRepository.java
+++ src/main/java/fr/mkadia/mkadiaapi/repositories/RoleRepository.java
@@ -2,11 +2,13 @@ package fr.mkadia.mkadiaapi.repositories;
 
 import fr.mkadia.mkadiaapi.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Integer> {
     List<Role> findAllByIdIn(List<Integer> IDs);
     Optional<Role> findFirstByLabelContainingOrderByLabelAsc(String label);
